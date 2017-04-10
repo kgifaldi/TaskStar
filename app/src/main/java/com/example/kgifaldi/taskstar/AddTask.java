d13e5908ff4926c776e8693cf7d5407c5e03b0f8
Kyle <kgifaldi@nd.edu>
2017-04-10 12:24:04 -0400

    Added Toggle Button to add task on parent side.

--- app/src/main/java/com/example/kgifaldi/taskstar/AddTask.java
+++ app/src/main/java/com/example/kgifaldi/taskstar/AddTask.java
@@ -2,6 +2,8 @@ package com.example.kgifaldi.taskstar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 /**
  * Created by Luigi on 4/8/17.
@@ -13,5 +15,33 @@ public class AddTask extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addtaskview);
+
+        final ToggleButton jt = (ToggleButton)findViewById(R.id.today_toggle);
+        final ToggleButton rt = (ToggleButton)findViewById(R.id.repeat_toggle);
+
+        jt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!jt.isChecked()){
+                    jt.setChecked(false);
+                    rt.setChecked(true);
+                    jt.setBackgroundTintList(getResources().getColorStateList(R.color.primaryText));
+                    rt.setBackgroundTintList(getResources().getColorStateList(R.color.text_icons));
+                }
+            }
+        });
+
+        rt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!rt.isChecked()){
+                    rt.setChecked(false);
+                    jt.setChecked(true);
+                    jt.setBackgroundTintList(getResources().getColorStateList(R.color.text_icons));
+                    rt.setBackgroundTintList(getResources().getColorStateList(R.color.primaryText));
+                }
+            }
+        });
+
     }
 }
